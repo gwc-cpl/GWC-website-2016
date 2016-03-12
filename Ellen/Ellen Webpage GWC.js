e238f20d134c3544acea51ab11b392e190a5f8e0
E Wang <erla63@gmail.com>
2016-03-11 22:11:31 -0500

    Added files via upload

---
+++ Ellen/Ellen Webpage GWC.js
@@ -0,0 +1,9 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+    "use strict";
+    $(".btn").click(function () {
+        //window.location =
+    });
+});
