df57965c71ecddfa02634729084e24a9a664192d
E Wang <erla63@gmail.com>
2016-03-16 17:59:06 -0400

    Update Ellen Webpage GWC.js

--- Ellen/Ellen Webpage GWC.js
+++ Ellen/Ellen Webpage GWC.js
@@ -3,7 +3,40 @@
 
 $(document).ready(function () {
     "use strict";
-    $(".btn").click(function () {
-        //window.location =
-    });
+	//variables
+	var y = 525;
+	var c = document.getElementById("myCanvas");
+	var ctx = c.getContext("2d");
+	var leftArrow = new Image()
+	var rightArrow = new Image()
+	var topArrow = new Image()
+	var bottomArrow = new Image()
+	var top = new Image();
+	leftArrow.src = "leftArrow.png";
+	top.src = "TopArrows.jpg";
+	top.onload = function(){       
+		ctx.drawImage(top,0,0);                 
+	};   
+	leftArrow.onload = function(){       
+		ctx.drawImage(leftArrow,0,y);                 
+	}; 
+
+	setInterval(function() {
+		//if (y == -10) {
+		//	if(){
+				
+		//	}
+			//clearInterval(id);
+		//}
+		ctx.rect(0,y,100,100);
+		ctx.fillStyle = "white";
+		ctx.fill();
+		if (y<100){
+			ctx.drawImage(top,0,0);
+		}
+		ctx.drawImage(leftArrow,0,y);
+		y--;
+	}, 10);
 });
+
+
